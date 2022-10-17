55940a2ebd8c8f9b724ab2088e39c86ff03a37dc
Jonas-Victor950 <111525829+Jonas-Victor950@users.noreply.github.com>
2022-10-17 16:03:37 -0300

    subindo rotas psicologos

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const atendimentoController = require('../controllers/atendimentoController');
+const psicologosController = require("../controllers/psicologosController")
+const validacaoPsicologos = require("../validations/psicologos/create")
 const routes = express.Router();
 
 routes.get("/atendimentos", atendimentoController.listarAtendimentos);
 routes.get("/atendimentos/:id", atendimentoController.listarAtendimento);
 routes.post("/atendimentos/criar", atendimentoController.criarAtendimento);
+routes.get("/psicologos", psicologosController.listarTodosPsicologos);
+routes.get("/psicologos/:id", psicologosController.listarUmPsicologo);
+routes.post("/psicologos", validacaoPsicologos, psicologosController.criarPsicologo);
+routes.put("/psicologos/:id", validacaoPsicologos, psicologosController.atualizarPsicologo);
+routes.delete("/psicologos/:id", psicologosController.deletarPsicologo);
 
 module.exports = routes;
