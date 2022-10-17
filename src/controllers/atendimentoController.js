50899d41523f09358c6edb8a02053b9bf617c1a6
elenbrodt <elenbrodt@gmail.com>
2022-10-17 15:31:47 -0300

    feat: listaAtendimentoId routes and controller

--- src/controllers/atendimentoController.js
+++ src/controllers/atendimentoController.js
@@ -6,6 +6,15 @@ const atenidmentoController = {
         const listaAtendimentos = await Atendimentos.findAll();
         res.json(listaAtendimentos);
     },
+    async listarAtendimento (req, res) {
+        const {id} = req.params;
+        const atendimentoId = await Atendimentos.findAll({
+            where: {
+                atendimento_id: id,
+              },
+        });
+        res.json(atendimentoId);
+    },
     async criarAtendimento(req, res) {
         const {paciente_id, psicologo_id, data_atendimento, abservacoes} = req.body;
         const atendimentoCriado = await Atendimentos.create({
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,8 @@ const express = require("express");
 const atendimentoController = require('../controllers/atendimentoController');
 const routes = express.Router();
 
-routes.get("/atendimentos/lista", atendimentoController.listarAtendimentos);
+routes.get("/atendimentos", atendimentoController.listarAtendimentos);
+routes.get("/atendimentos/:id", atendimentoController.listarAtendimento);
 routes.post("/atendimentos/criar", atendimentoController.criarAtendimento);
 
 module.exports = routes;
